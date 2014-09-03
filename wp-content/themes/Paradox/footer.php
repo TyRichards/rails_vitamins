c6275a234f1086647520d89cbf70325ea3204c5e
Ty Richards <me@tyrichards.com>
2014-09-03 15:54:40 -0500

    Add ReMarketing Javascript

--- wp-content/themes/Paradox/footer.php
+++ wp-content/themes/Paradox/footer.php
@@ -56,6 +56,19 @@
 		    var s = document.getElementsByTagName('script')[0];
 		    s.parentNode.insertBefore(dc, s);
 		  })();
-		</script>		
+		</script>	
+
+		<!-- Perfect Audience -->
+		<script type="text/javascript">
+		  (function() {
+		    window._pa = window._pa || {};
+		    // _pa.orderId = "myOrderId"; // OPTIONAL: attach unique conversion identifier to conversions
+		    // _pa.revenue = "19.99"; // OPTIONAL: attach dynamic purchase values to conversions
+		    // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
+		    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+		    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.perfectaudience.com/serve/54077f2a5ded377179000145.js";
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
+		  })();
+		</script>			
 	</body>
 </html>
