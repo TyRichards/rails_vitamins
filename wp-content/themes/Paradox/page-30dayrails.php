b41c30561f53765ed7fd4672aa6a64e6d4d619f7
Ty Richards <me@tyrichards.com>
2014-09-04 23:36:52 -0500

    update pitch page for presale

--- wp-content/themes/Paradox/page-30dayrails.php
+++ wp-content/themes/Paradox/page-30dayrails.php
@@ -48,36 +48,72 @@ Template Name: 30-Day Rails
                 ?>                                                
             </article>  
         </div> <!-- .row -->             
+
+        <!-- Temporary -->
         <div class="row">
-<!--             <div class="col-md-7">
-                <div class="well well-lg">
-                    <div class="clearfix">
-                        <h3>Pre-Order and Pay What You Want</h3>
-                        <ul>
-                            <li>Early access to each lesson as I complete them</li>
-                            <li>One year of access to the online course (after launch)</li>
-                            <li>Source code to the demo app</li>
-                            <li>PDF format of lessons</li>
-                        </ul>
-
-                        <a class="btn btn-primary btn-block" href="/pre-order">Pre-Order Now</a>
-
-Save by joining early. Price will double on launch day. This will get you:
-
-This only lasts until the official launch of the product in a few weeks. Plus when you pre-order Rails Curve, I can make the initial course that much better.
-
-
-                        
-                    </div> .clearfix -->
-      
             <div class="col-md-6 col-md-offset-3">                
                 <div class="well well-lg text-center">
                     <div class="clearfix">
-                        <h4>Get updates on when 30-Day Rails is released</h4>
+                        <h3>Get update when <i>30-Day Rails</i> is released</h3>
                         <?php gravity_form(3, false, true, false, null, true, 50); ?>
                     </div>
                 </div>                    
             </div>
+        </div>
+
+        <!-- Pre-Order -->
+        <div class="row">
+            <div class="col-md-8">
+                <div class="well well-lg" style="display:none;">
+                    <div class="clearfix">
+                        <h2>Pre-Order and Pay What You Want</h2>
+                        
+                        <?php gravity_form(4, false, true, false, null, true, 100); ?>   
+                        <hr>
+                        <div class="col-md-6">
+                            <strong>If you pay from $1 to $49</strong>
+                            <div class="small">
+                                <p>Only want the book, pay any price, and you'll get the book as well as the source code, starting you on your journey to understanding how to develop iOS and OS X applications using RubyMotion!</p>
+
+                                <p>I'll show you straightforward ways to look at iOS and OS X development from a Rails point of view, but don't worry, though it will be an effortless journey, this won't be just an introductory book.</p>
+
+                                <p><strong>Formats:</strong> PDF, ePub, and Mobi</p>
+                            </div>
+                        </div> 
+                        <div class="col-md-6">
+                            <strong>If you pay over $50</strong>
+                            <div class="small">                            
+                                <p>If you choose this package, not only are you further supporting me in helping the community, but I'll also be giving you screencasts that I'll make to go along with this book and take things beyond the confines of text!</p>
+
+                                <p>You'll get the same "easy to follow but certainly not lacking the details" book, though you will be able to dive deeper into the topics covered, by watching these screencasts.</p>
+
+                                <p><strong>Formats:</strong> MP4, PDF, ePub, and Mobi</p>
+                            </div>
+                        </div>                         
+
+                        <!-- <ul>
+                            <li>Early access to each lesson as I complete them</li>
+                            <li>One year of access to the online course (after launch)</li>
+                            <li>Source code to the demo app</li>
+                            <li>PDF format of lessons</li>
+                        </ul>                        
+
+                        Save by joining early. Price will double on launch day. This will get you:
+
+                        This only lasts until the official launch of the product in a few weeks. Plus when you pre-order Rails Curve, I can make the initial course that much better. -->
+                 
+                    </div><!-- .clearfix -->
+                </div>  
+            </div>          
+            <div class="col-md-4">                
+                <div class="well well-lg text-center" style="display:none">
+                    <div class="clearfix">
+                        <h4><strong>OR</strong> get updates on when 30-Day Rails is released</h4>
+                        <?php gravity_form(3, false, true, false, null, true, 50); ?>
+                    </div>
+                </div>                    
+            </div>
+
         </div> <!-- .row -->        
     </div> <!-- .container -->
 </section> <!-- .primary-content -->
