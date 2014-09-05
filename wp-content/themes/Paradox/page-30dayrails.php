d04cbb762a0bdbe3661b306d8c575576531a2bb7
Ty Richards <me@tyrichards.com>
2014-09-05 17:08:19 -0500

    Added pre-launch info

--- wp-content/themes/Paradox/page-30dayrails.php
+++ wp-content/themes/Paradox/page-30dayrails.php
@@ -50,63 +50,67 @@ Template Name: 30-Day Rails
         </div> <!-- .row -->             
 
         <!-- Temporary -->
-        <div class="row">
+<!--         <div class="row">
             <div class="col-md-6 col-md-offset-3">                
                 <div class="well well-lg text-center">
                     <div class="clearfix">
                         <h3>Get update when <i>30-Day Rails</i> is released</h3>
-                        <?php gravity_form(3, false, true, false, null, true, 50); ?>
+                        <?php //gravity_form(3, false, true, false, null, true, 50); ?>
                     </div>
                 </div>                    
             </div>
-        </div>
+        </div> -->
 
         <!-- Pre-Order -->
         <div class="row">
             <div class="col-md-8">
-                <div class="well well-lg" style="display:none;">
+                <div class="well well-lg" >
                     <div class="clearfix">
-                        <h2>Pre-Order and Pay What You Want</h2>
+                        <h2 class="text-center">Pre-order & pay what you want</h2>
                         
-                        <?php gravity_form(4, false, true, false, null, true, 100); ?>   
-                        <hr>
-                        <div class="col-md-6">
-                            <strong>If you pay from $1 to $49</strong>
-                            <div class="small">
-                                <p>Only want the book, pay any price, and you'll get the book as well as the source code, starting you on your journey to understanding how to develop iOS and OS X applications using RubyMotion!</p>
-
-                                <p>I'll show you straightforward ways to look at iOS and OS X development from a Rails point of view, but don't worry, though it will be an effortless journey, this won't be just an introductory book.</p>
-
-                                <p><strong>Formats:</strong> PDF, ePub, and Mobi</p>
+                        <div class="row">     
+                            <div class="col-md-8 col-md-offset-2">
+                                <?php gravity_form(4, false, true, false, null, true, 100); ?>   
                             </div>
-                        </div> 
-                        <div class="col-md-6">
-                            <strong>If you pay over $50</strong>
-                            <div class="small">                            
-                                <p>If you choose this package, not only are you further supporting me in helping the community, but I'll also be giving you screencasts that I'll make to go along with this book and take things beyond the confines of text!</p>
+                        </div>   
+                        <hr>             
+                        <div class="row">                                                                
+                            <div class="col-md-6">
+                                <h3 class=""><i class="fa fa-question-circle"></i> How it works</h3>
+                                <div class="small">
+                                    <p>It's a no-nonsense deal, pick the amount you want to pay for the online course, anything between $10 to $1000, completely up to you. In return, you'll get access to the parts of the course as it's created, and the final version upon it’s completion.</p>
 
-                                <p>You'll get the same "easy to follow but certainly not lacking the details" book, though you will be able to dive deeper into the topics covered, by watching these screencasts.</p>
+                                    <p>If you pay less than $50 then you will get online access to each lesson. If you pay over $50 or more then you'll get access to the screencasts that go with each lesson.</p>
 
-                                <p><strong>Formats:</strong> MP4, PDF, ePub, and Mobi</p>
-                            </div>
-                        </div>                         
-
-                        <!-- <ul>
-                            <li>Early access to each lesson as I complete them</li>
-                            <li>One year of access to the online course (after launch)</li>
-                            <li>Source code to the demo app</li>
-                            <li>PDF format of lessons</li>
-                        </ul>                        
-
-                        Save by joining early. Price will double on launch day. This will get you:
-
-                        This only lasts until the official launch of the product in a few weeks. Plus when you pre-order Rails Curve, I can make the initial course that much better. -->
-                 
-                    </div><!-- .clearfix -->
-                </div>  
-            </div>          
+                                    <p>If you're interested, but not sure about it, maybe just pay $10, then as you start getting updated with more lessons, if you decide it's worth a greater amount, you can pay more, or if you're unhappy, you can just get a refund.</p>
+                                </div>
+                            </div>  
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h5><strong><i class="fa fa-wrench"></i> If you pay from $1 to $49</strong></h5>                                
+                                        <div class="small">
+                                            <p><strong>Format:</strong> Online Course</p>
+                                            <p>Only want the course with no screencasts, pay any price, and you'll get the course as well as the source code, starting on your journey to understanding how to develop Rails apps.</p>                                    
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div class="row" style="margin-top:30px;">
+                                    <div class="col-md-12">
+                                        <h5><strong><i class="fa fa-youtube-play"></i> If you pay over $50</strong></h5>
+                                        <div class="small">                  
+                                            <p><strong>Format:</strong> Online Course + Screencasts</p>
+                                            <p>You'll get the same "easy to follow but certainly not lacking the details" course, though you will be able to dive deeper into the topics covered, by watching these screencasts. Not to mention, the more you pay the more time I can dedicate to the community and to making this course that much more awesome.</p>
+                                        </div>
+                                    </div> 
+                                </div>                                
+                            </div>                 
+                        </div> <!-- .row -->                                         
+                    </div> <!-- .clearfix -->
+                </div>  <!-- .well -->
+            </div> <!-- .col -->
             <div class="col-md-4">                
-                <div class="well well-lg text-center" style="display:none">
+                <div class="well well-lg text-center">
                     <div class="clearfix">
                         <h4><strong>OR</strong> get updates on when 30-Day Rails is released</h4>
                         <?php gravity_form(3, false, true, false, null, true, 50); ?>
