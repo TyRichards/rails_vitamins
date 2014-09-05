6ee44111312c5c6d0d30b8182653637cfab083db
Ty Richards <me@tyrichards.com>
2014-09-04 19:56:44 -0500

    add Yoast SEO, change 404 and header

--- wp-content/themes/Paradox/404.php
+++ wp-content/themes/Paradox/404.php
@@ -1,19 +1,14 @@
 <?php get_header(); ?> 
 
-<section class="masthead" style="min-height:600px">
-    <div class="background">&nbsp;</div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 col-sm-offset-2 text-center">
-                <h1>Oops, nothing to see here</h1>            
-                
-	            <div class="main-col col-md-8 col-md-offset-2 page-content text-center">	            	
-	            	<a class="btn btn-primary" href="/">Read The Paradox Journal</a>
-	            	<a class="btn btn-primary" href="/start">Start a Project</a>
-	            </div> <!-- .main-col -->                
-            </div>
-        </div>
-    </div>
-</section>	
+<section class="primary-content">
+    <div class="container">     
+        <div class="row">    
+            <article class="main-col col-sm-10 col-sm-offset-1 page-content text-center">   
+                <h1>Oops, nothing to see here</h1>            	            
+            	<a class="btn btn-primary" href="/railsvitamins">Read Rails Vitamins</a>	            
+            </article>  
+        </div> <!-- .row -->             
+    </div> <!-- .container -->
+</section> <!-- .primary-content -->
 
 <?php get_footer(); ?> 
